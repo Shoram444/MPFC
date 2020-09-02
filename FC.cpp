587632daff0c17487bd64dae80246895da1220ff
shoram444 <petro.maros.afs@gmail.com>
2020-09-02 15:05:05 +0200

    nothing really

--- FC.cpp
+++ FC.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main() 
 { 
-    MPFeldman_Cousins* obj = new MPFeldman_Cousins(3.0, 1, 10, 10, 0.95);   // order for parameters (double _b, double _step, int _rows, int _mu_max, double _CL)
+    MPFeldman_Cousins* obj = new MPFeldman_Cousins(3.0, 0.01, 1000, 10, 0.95);   // order for parameters (double _b, double _step, int _rows, int _mu_max, double _CL)
 
     obj->get_n();
     // obj->draw_upper();
